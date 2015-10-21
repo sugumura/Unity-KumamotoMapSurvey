f8311a21c40ffbd6697c7643ce316e560a0af611
Suguru Murakami <sugumura@gmail.com>
2015-10-22 00:52:15 +0900

    add Parse.comへのテスト実装を作成

---
+++ Assets/Scripts/TestParse.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using Parse;
+
+public class TestParse : MonoBehaviour {
+
+    void Start() {
+        ParseObject testObject = new ParseObject("TestObject");
+		testObject["foo"] = "bar";
+		testObject.SaveAsync();
+    }
+
+}
