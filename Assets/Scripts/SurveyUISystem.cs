1b25151fa3a4e15d8dbf72567c92881afc529590
Suguru Murakami <sugumura@gmail.com>
2015-10-22 16:09:31 +0900

    add 画面遷移を実装

---
+++ Assets/Scripts/SurveyUISystem.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SurveyUISystem : MonoBehaviour {
+	
+	[SerializeField]
+	private GameObject firstPanel;
+	
+	[SerializeField]
+	private GameObject secondPanel;
+	
+	void Start() {
+		Debug.Log("Start");
+		ShowFirstPanel();
+	}
+	
+	void ShowFirstPanel() {
+		firstPanel.SetActive(true);
+		secondPanel.SetActive(false);
+	}
+	
+	void ShowSecondPanel() {
+		firstPanel.SetActive(false);
+		secondPanel.SetActive(true);
+	}
+	
+	public void Next() {
+		Debug.Log("Next");
+		ShowSecondPanel();
+	}
+	
+	public void Back() {
+		Debug.Log("Back");
+		ShowFirstPanel();
+	}
+	
+	public void Complete() {
+		Debug.Log("Complete");
+		
+	}
+}
